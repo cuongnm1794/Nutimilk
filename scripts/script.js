1fdeca69a2d4729b02eb8f145df5f0db4973d4a4
Cuong <cuongnm1794@gmail.com>
2022-12-13 00:01:16 +0700

    Update css pc + phone

--- scripts/script.js
+++ scripts/script.js
@@ -13,17 +13,10 @@ let height1;
 
 console.log(document.getElementById("footer"));
 
-if (document.getElementById("qrcode")) {
-  height1 =
-    document.getElementById("qrcode").offsetHeight +
-    document.getElementById("qrcode").getBoundingClientRect().top +
-    window.scrollY;
-} else {
-  height1 =
-    document.getElementById("footer").offsetHeight +
-    document.getElementById("footer").getBoundingClientRect().top +
-    window.scrollY;
-}
+height1 =
+  document.getElementById("footer").offsetHeight +
+  document.getElementById("footer").getBoundingClientRect().top +
+  window.scrollY;
 console.log(height1);
 
 document.getElementById("background__img").style.height = height1 + "px";
