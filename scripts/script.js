d23162604183237845b95e1c46fd5a4d6d162fbb
Cuong <cuongnm1794@gmail.com>
2022-12-13 11:23:57 +0700

    Update script resize window

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,26 @@
-var body = document.body,
-  html = document.documentElement;
+function updateSizeBackground() {
+  var body = document.body,
+    html = document.documentElement;
 
-var height = Math.max(
-  body.scrollHeight,
-  body.offsetHeight,
-  html.clientHeight,
-  html.scrollHeight,
-  html.offsetHeight
-);
+  var height = Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  );
 
-let height1;
+  let height1;
 
-console.log(document.getElementById("footer"));
+  height1 =
+    document.getElementById("footer").offsetHeight +
+    document.getElementById("footer").getBoundingClientRect().top +
+    window.scrollY;
 
-height1 =
-  document.getElementById("footer").offsetHeight +
-  document.getElementById("footer").getBoundingClientRect().top +
-  window.scrollY;
-console.log(height1);
+  document.getElementById("background__img").style.height = height1 + "px";
+}
 
-document.getElementById("background__img").style.height = height1 + "px";
+addEventListener("resize", (event) => {
+  updateSizeBackground();
+});
+updateSizeBackground();
